6f98427ae7150bdade41e74814983883c7a52633
linxiaogang <linxiaogang@badousoft.com>
2020-05-23 18:48:03 +0800

    开发自己的主题，加入了文章详情页面；

--- themes/gun/source/js/main.js
+++ themes/gun/source/js/main.js
@@ -2,7 +2,7 @@
  * @Description: gun - 主js
  * @Author: LXG
  * @Date: 2020-05-21
- * @LastEditTime: 2020-05-21
+ * @LastEditTime: 2020-05-23
  */
 $(document).ready(function () {
     console.log('ready js')
